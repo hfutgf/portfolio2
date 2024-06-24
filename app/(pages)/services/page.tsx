8178e115d02e08c0fafcbd992ffb95aa06f5ea8e
hfutgf <beretax308@gmail.com>
2024-06-24 22:33:28 +0500

    bug fix

--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -9,7 +9,7 @@ import { fadeIn } from "@/variant";
 const ServicePage = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
-      <Circles />
+      {/* <Circles />
       <div className=" container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           <motion.div
@@ -38,7 +38,7 @@ const ServicePage = () => {
           </motion.div>
         </div>
       </div>
-      <Bulb />
+      <Bulb /> */}
     </div>
   );
 };
