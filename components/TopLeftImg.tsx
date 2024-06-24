c8c1914e04776c5c946ad03993f41deb3e536542
hfutgf <beretax308@gmail.com>
2024-06-24 23:37:53 +0500

    bug with button fixed

--- components/TopLeftImg.tsx
+++ components/TopLeftImg.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
-      <Image src={"/top-left-img.png"} alt="Image" width={400} height={400} />
+      <Image src={"/png/top-left-img.png"} alt="Image" width={400} height={400} />
     </div>
   );
 };
